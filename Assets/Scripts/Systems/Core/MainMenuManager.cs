e8decde41cd70e3cfc6647f7b96c31030345d0c4
Tristan Phipson <tristanphip26@gmail.com>
2024-08-22 22:54:15 +0200

    Menu
    yes

--- Assets/Scripts/Systems/Core/MainMenuManager.cs
+++ Assets/Scripts/Systems/Core/MainMenuManager.cs
@@ -3,11 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Pannels")]
+    [SerializeField] GameObject MainMenuPanel = null;
+    [SerializeField] GameObject LevelSelectPanel = null;
+    [SerializeField] GameObject MinigameSelectPanel = null;
+    [SerializeField] GameObject HowToPlayPanel = null;
+
+    const string START_LEVEL_NAME = "Level 1";
+
     private GameManager gameManager;
 
     void Start()
     {
         gameManager = GameManager.Instance;
+    }
+    public void StartGame()
+    {
+        gameManager.LoadLevel(START_LEVEL_NAME);
     }
 
     public void LoadLevel(string levelSceneName)
@@ -18,5 +30,31 @@ public class MainMenuManager : MonoBehaviour
     public void LoadMinigame(string minigameSceneName)
     {
         gameManager.LoadMinigame(minigameSceneName, false, null);
+    }
+
+    public void LevelSelectButtonHandler()
+    {
+        MainMenuPanel.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+    }
+
+    public void MiniGameSelectButtonHandler()
+    {
+        MainMenuPanel.SetActive(false);
+        MinigameSelectPanel.SetActive(true);
+    }
+
+    public void HowToPlayButtonHandler()
+    {
+        MainMenuPanel.SetActive(false);
+        HowToPlayPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        MainMenuPanel.SetActive(true);
+        MinigameSelectPanel.SetActive(false);
+        HowToPlayPanel.SetActive(false);
+        LevelSelectPanel.SetActive(false);
     }
 }
